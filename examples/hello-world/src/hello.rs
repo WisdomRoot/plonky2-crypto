bb24dd06a5d3936699cd284c6752ecfe00571caa
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2025-05-21 13:12:43 +0800

    run sha3-256 in example

--- examples/hello-world/src/hello.rs
+++ examples/hello-world/src/hello.rs
@@ -36,36 +36,37 @@ mod tests {
     use plonky2::plonk::circuit_builder::CircuitBuilder;
     use plonky2::plonk::circuit_data::CircuitConfig;
     use plonky2::plonk::config::*;
-    use plonky2_crypto::hash::sha256::WitnessHashSha2;
+    use plonky2_crypto::hash::{keccak256::{CircuitBuilderHashKeccak, WitnessHashKeccak, KECCAK256_R}, sha256::WitnessHashSha2};
     use std::time::{Duration, Instant};
 
     use super::*;
 
     #[test]
-    fn test_double_sha256() {
+    fn test_keccak_sha256() {
         let tests = [
             [
                 // 64 bytes input
-                "600D54000000000000000000000000000000000000000000000000000000000077F1040000000000000000000000000000000000000000000000000000000000",
-                // sha256(sha256(input))
-                "29ee05d175e91556b92d7c17919d42247a53096f5b58250faa3ca6ad5cbcefa7",
+                "",
+                "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
             ],
             // [
             //     "...",
             //     "...",
             // ],
         ];
+        let _ = env_logger::builder().format_timestamp(None).try_init();
 
         const D: usize = 2;
         type C = PoseidonGoldilocksConfig;
         type F = <C as GenericConfig<D>>::F;
-        let config = CircuitConfig::standard_recursion_config();
+        let config = CircuitConfig::standard_recursion_zk_config();
         let mut builder = CircuitBuilder::<F, D>::new(config);
 
         // 0. create the circuit
-        let target_input = builder.add_virtual_hash_input_target(2, 512);
-        let target_output = builder.double_sha256(&target_input);
+        let target_input = builder.add_virtual_hash_input_target(1, KECCAK256_R);
+        let target_output = builder.hash_keccak256(&target_input);
         let num_gates = builder.num_gates();
+        builder.print_gate_counts(0);
 
         // 1. build circuit once
         let now = Instant::now();
@@ -74,26 +75,44 @@ mod tests {
 
         // 2. generate multiple ZKPs, one per test
         let mut time_prove = Duration::new(0, 0);
+        let mut time_verify = Duration::new(0, 0);
         for t in tests {
             let input = hex::decode(t[0]).unwrap();
             let output = hex::decode(t[1]).unwrap();
 
             // set input/output
             let mut pw = PartialWitness::new();
-            pw.set_sha256_input_target(&target_input, &input);
-            pw.set_sha256_output_target(&target_output, &output);
+            pw.set_keccak256_input_target(&target_input, &input).unwrap();
+            pw.set_keccak256_output_target(&target_output, &output).unwrap();
 
             // generate proof
             let now = Instant::now();
             let proof = data.prove(pw).unwrap();
-            time_prove += now.elapsed();
+            let tmp = now.elapsed();
+
+            time_prove += tmp;
 
             // verify proof
-            assert!(data.verify(proof).is_ok());
+            data.verify(proof.clone()).unwrap();
+            time_verify += now.elapsed() - tmp;
+
+            println!("proof size: {:.2} KB", proof.to_bytes().len() as f64/1024.0);
+
+            let pf = proof.compress(&data.verifier_only.circuit_digest, &data.common).unwrap();
+            let pf_comp_size = pf.to_bytes().len() as f64/1024.0;
+            println!("compress proof size: {:.2} KB", pf_comp_size);
+
+            for g in data.common.gates.iter() {
+                println!("({:?}): {}", g, g.0.num_constraints());
+            }
+
         }
         time_prove /= tests.len() as u32;
+        time_verify /= tests.len() as u32;
         println!(
-            "double_sha256 num_gates={num_gates} time_build={time_build:?} time_prove={time_prove:?}"
+            "sha3-256 num_gates={num_gates} time_build={time_build:?} time_prove={time_prove:?} time_verify={time_verify:?}"
         );
+
+
     }
 }
